f430e155807a7324199070084019a5db211e87bc
Arielle <arielle.palmer12@gmail.com>
2026-01-16 23:30:44 -0600

    Stage 1: scaffold Vite project, setup directories, clean App.jsx, add normalize.css

---
+++ rooted-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+import "./vendor/normalize.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rooted Frontend - Stage 1</h1>
+    </div>
+  );
+}
+
+export default App;
